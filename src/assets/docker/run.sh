f66d73bd70d383403ec0ee0e94938558f5f533a4
Dave <dave@example.com>
2024-03-02 11:50:13 -0500

    Fix actions clean, sync, add list, use buildkit
    -Change clean to reset, fix to clean everything
    -Fix sync to restart if sync was itself updated
    -Add list [stats] to show all containers/images
    -Use buildkit

--- src/assets/docker/run.sh
+++ src/assets/docker/run.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 main() {
+  sync "$@"
+
   OPT="${1:-}"
   shift
 
   if [[ "$OPT" == "status" ]]; then
     _call status
-  elif [[ "$OPT" == "clean" ]]; then
-    _call clean
+  elif [[ "$OPT" == "reset" ]]; then
+    _call reset
+    _call list
+  elif [[ "$OPT" == "list" ]]; then
+    _call list "$@"
   elif [[ "$OPT" == "recreate" ]]; then
-    _call run_container recreate
+    _call clean_dev_container
+    _call run_container
   elif [[ "$OPT" == "" ]]; then
     _call run_container
   else
@@ -18,38 +24,58 @@ main() {
 }
 
 usage() {
-  echo "| Usage: ${ALIAS_USED:-$0} [status|clean|recreate|help]"
+  echo "| Usage: ${ALIAS_USED:-$0} [status|reset|list [stats]|recreate|help]"
+  echo "| (If found, $script_path/sync.sh will run first.)"
   echo "| When no argument is provided:"
   echo "|   -Run or reattach to the $dev_container"
-  echo "|    (If found, $script_path/sync.sh will run first.)"
   echo "| Optional arguments:"
-  echo "|   status: Check if the $dev_container is running"
-  echo "|   clean: Purge related containers and images"
-  echo "|   recreate: Force rebuild of the $dev_container"
+  echo "|   status: Report if the $dev_container is running"
+  echo "|   reset: Stop and purge all containers and images"
+  echo "|   list [stats]: List all containers, images, and optional stats"
+  echo "|   recreate: Rebuild the $dev_container image and container"
   echo "|   help: Show this usage info"
   return 1
 }
 
+reset() {
+  ids=$(docker ps -aq)
+  if [[ "$ids" ]]; then
+    # shellcheck disable=SC2086
+    docker container stop $ids || true
+  fi
+  docker container prune -f || true
+  docker image prune -af || true
+}
+
+clean_dev_container() {
+  docker compose down --rmi all -t 2 "$dev_container" || true
+}
+
 run_container() {
-  status || true
-  local recreate=
-  sync "$@"
   run_socat
-  [ "$1" == 'recreate' ] && recreate='--force-recreate'
-  docker compose --profile "$dev_container" up -d $recreate
+  docker compose up -d "$dev_container"
   docker compose exec -it "$dev_container" bash
 }
 
-clean() {
-  docker rm -f socat 2>/dev/null || true
-  docker rmi -f alpine/socat 2>/dev/null || true
-  docker compose down --rmi all -t 0 "$dev_container"
+list() {
+  HIGHLIGHT="\x1b[33;44m"
+  NC='\033[0m'
+  echo -e "\n${HIGHLIGHT} images ${NC}"
+  docker image ls -a
+  echo -e "\n${HIGHLIGHT} containers ${NC}"
+  docker container ls -a
+  if [[ "$1" == "stats" ]]; then
+    echo -e "\n${HIGHLIGHT} stats ${NC}"
+    echo -en "wait\r"
+    docker stats --no-stream
+  fi
+  echo
 }
 
 run_socat() {
   if ! docker inspect -f '{{.State.Running}}' "socat" &>/dev/null; then
     echo "Launching socat"
-    docker rm -f socat 2>/dev/null || true
+    docker rm -f socat >/dev/null 2>&1 || true
     docker run --rm -d --name socat \
       -p 127.0.0.1:2376:4000 \
       -v /var/run/docker.sock:/var/run/docker.sock \
@@ -63,15 +89,17 @@ run_socat() {
 sync() {
   script="$script_path/sync.sh"
   if [ -f "$script" ]; then
-    echo "Running sync.sh"
-    initial_timestamp=$(stat -c %Y "$0")
+    echo -n "Running sync.sh"
+    initial_timestamp1=$(stat -c %Y "$0")
+    initial_timestamp2=$(stat -c %Y "$script")
     $script
-    current_timestamp=$(stat -c %Y "$0")
-    if [ "$current_timestamp" -gt "$initial_timestamp" ]; then
-      echo "Script has been updated. Relaunching ${ALIAS_USED:-$0}" "$@"
-      exec "$0" "$@"
+    current_timestamp1=$(stat -c %Y "$0")
+    current_timestamp2=$(stat -c %Y "$script")
+    if [[ "$current_timestamp1" -gt "$initial_timestamp1" || "$current_timestamp2" -gt "$initial_timestamp2" ]]; then
+      echo ". Script updated. Relaunching..."
+      exec "$0" "$*"
     else
-      echo "Script was not updated. Continuing"
+      echo ". Script not updated. Continuing..."
     fi
   fi
 }
@@ -92,9 +120,11 @@ dev_container=devcontainer
 uid=$(id -u)
 gid=$(id -g)
 
+export DOCKER_BUILDKIT=1
 export COMPOSE_FILE="$script_path/compose.yaml"
 export COMPOSE_FILE_PATH="$script_path"
 export WSL_UID="$uid"
 export WSL_GID="$gid"
+export SRC_PATH_FRONTEND=~/src/ng1/frontend
 
 main "$@"
