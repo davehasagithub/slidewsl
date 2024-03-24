5422aa9dba421281fd3bac4c88e4f2344e8491e4
Dave <dave@example.com>
2024-03-24 08:58:45 -0400

    Update dev-admin usage

--- src/assets/slidewsl/dev-admin.sh
+++ src/assets/slidewsl/dev-admin.sh
@@ -20,31 +20,31 @@ main() {
 }
 
 usage() {
-  cat <<EOF | sed "s/^    //"  | daveml.sh -p "| "
+  cat <<EOF | sed "s/^    //"  | /usr/local/bin/daveml.sh -p "| "
 
     <WBB>   Welcome to the SlideWSL development environment!   <CLR>
 
-    <GXX>Redisplay this info  <KXW> dev <CLR> (or ~/slidewsl/dev-admin.sh)
+    <CBX>To redisplay this info, type <CLR><KXW> dev <CLR> (or ~/slidewsl/dev-admin.sh)
 
-    <GBX>Launch environment   <CLR><CXX>docker compose up -d
-    <GBX>Webpack dev server   <CLR><CXX>APPS="<YBX><app> <CLR><YXX>[...]<CXX>" docker compose up --force-recreate angular_dev_server -d
-    <GBX>Tail the logs        <CLR><CXX>docker compose logs -f
-    <GBX>Build an angular app <CLR><CXX>docker compose run --rm angular build <YBX><app> <CLR><YXX>[<base-href> [<other-args...>]]
+    <GBX>Launch the environment <CLR><CXX>docker compose up -d
+    <GBX>Tail the service logs  <CLR><CXX>docker compose logs -f
+    <GBX>Run webpack dev server <CLR><CXX>APPS="<YBX><app> <CLR><YXX>[...]<CXX>" docker compose up --force-recreate angular_dev_server -d
+    <GBX>Compile an angular app <CLR><CXX>docker compose run --rm angular build <YBX><app> <CLR><YXX>[<base-href> [<other-args...>]]
+    <GBX>See what's running     <CLR><CXX>docker compose ps
+    <GBX>Stop all services      <CLR><CXX>docker compose --profile "*" down
 
-    <WXX>See what's running   <CLR><CXX>docker compose ps
-    <WXX>Stop environment     <CLR><CXX>docker compose down
-    <WXX>Update node_modules  <CLR><CXX>docker compose run --rm angular node_modules
-    <WXX>Update composer      <CLR><CXX>docker compose run --rm php composer
-    <WXX>Interactive terminal <CXX>docker compose exec -it -u root <YXX><service><CXX> bash
-    <WXX>Tail laravel log     <CXX>docker compose exec php-fpm tail -f /laravel/storage/logs/laravel_line-202x-xx-xx.log
-    <WXX>Install in container <CXX>apt update; apt install -y <YXX>iputils-ping telnet vim less
-    <WXX>Make angular starter <CXX>docker compose run --rm angular starter <YXX><app>
-    <WXX>Make laravel starter <CXX>docker compose run --rm php starter
-    <WXX>Check keydb cluster  <CXX>docker compose exec -it keydb-node1 keydb-cli cluster info
-    <WXX>Rebuild services     <CXX>docker compose down --rmi all --remove-orphans -v && docker compose build --no-cache
-    <WXX>Run sync.sh script   <CXX>${ALIAS_USED:-$0} sync
-    <WXX>See Docker resources <CXX>${ALIAS_USED:-$0} list [stats]
-    <WXX>Reset                <CXX>${ALIAS_USED:-$0} reset [cache]
+    <WXX>Tail a laravel log     <CXX>docker compose exec php-fpm tail -f /laravel/storage/logs/laravel_line-<YXX>YYYY<CXX>-<YXX>MM<CXX>-<YXX>DD<CXX>.log
+    <WXX>Update node_modules    <CXX>docker compose run --rm angular node_modules
+    <WXX>Update composer        <CXX>docker compose run --rm php composer
+    <WXX>Make angular starter   <CXX>docker compose run --rm angular starter <YXX><app>
+    <WXX>Make laravel starter   <CXX>docker compose run --rm php starter
+    <WXX>Check keydb cluster    <CXX>docker compose exec -it keydb-node1 keydb-cli cluster info
+    <WXX>Interactive terminal   <CXX>docker compose exec -it -u root <YXX><service><CXX> bash
+    <WXX>                        ⤷ then, for example: <CXX>apt update; apt install -y <YXX>iputils-ping telnet vim less
+
+    <WXX>Run sync.sh script     <CXX>${ALIAS_USED:-$0} sync
+    <WXX>See Docker resources   <CXX>${ALIAS_USED:-$0} list <YXX>[stats]
+    <WXX>Reset                  <CXX>${ALIAS_USED:-$0} reset <YXX>[cache]
 
 EOF
 
