a16428da49ded2bcdcbe9c1019d2a067393f7c06
Dave <dave@example.com>
2024-06-19 07:22:42 -0400

    Update example sync script

--- local/example_sync.sh
+++ local/example_sync.sh
@@ -6,13 +6,16 @@ root=/mnt/c/users/dave/Desktop/git/slidewsl
   rsync -a --out-format='%n%L' $root/src/assets/slidewsl/ ~/slidewsl --delete
 
   echo "add customizations:"
-  cp -a $root/local/dev-server.conf ~/slidewsl/angular/conf
-  cp -a $root/local/docker-custom.env ~/slidewsl
-  cp -a $root/local/docker-php.env ~/slidewsl
-  cp -a $root/local/docker-phpmyadmin.env ~/slidewsl
+  cp -a $root/local/dev-server.conf ~/slidewsl/node/conf
   cp -a $root/local/php.extras.ini ~/slidewsl/php/conf
-  cp -a $root/local/browscap.ini ~/slidewsl/php/scripts
+  cp -a $root/local/lite_php_browscap.ini ~/slidewsl/php/etc
   cp -a $root/local/sync.sh ~/slidewsl
+  cp -a $root/local/cert.* ~/slidewsl/nginx/certs
+
+  go run ~/slidewsl/_templates/cmd/render.go --env-file ~/slidewsl/_env/local.env --environment local --workspace-path ~/slidewsl; \
+  go run ~/slidewsl/_templates/cmd/render.go --env-file ~/slidewsl/_env/build.env --environment build --workspace-path ~/slidewsl; \
+  go run ~/slidewsl/_templates/cmd/render.go --env-file ~/slidewsl/_env/staging.env --environment staging --workspace-path ~/slidewsl; \
+  cp ~/slidewsl/compose*.yaml $root/src/assets/slidewsl/
 
   echo "dos2unix:"
   find ~/slidewsl -type f \( -not -name \*.enc \) -exec dos2unix -ic0 {} + | xargs -0 dos2unix
