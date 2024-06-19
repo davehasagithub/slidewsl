29f4988004492749960b477707b7b3fc401d3b44
Dave <dave@example.com>
2024-06-19 06:59:01 -0400

    Add ij1, ij2 aliases for launching intellij

--- src/assets/wsl/wsl-aliases.sh
+++ src/assets/wsl/wsl-aliases.sh
@@ -4,3 +4,6 @@ dev() { (
 ); }
 
 alias daveml="/usr/local/bin/daveml.sh"
+
+alias ij1="~/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin/idea.sh >/dev/null 2>&1 &"
+alias ij2="./.local/share/JetBrains/idea-IU-241.17890.1/bin/idea.sh >/dev/null 2>&1 &"
